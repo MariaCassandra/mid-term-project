1957cae66a6ad18c696955779041af40a30e23c5
Maria Cassandra <maria.cassandra@gmail.com>
2022-08-09 20:36:01 +0200

    js correction and img fix

--- scr/index.js
+++ scr/index.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
@@ -7,33 +7,11 @@ function myFunction() {
     }
   }
 
-
-/*
-contact page:
-<button class="contact-submit-btn" onclick="clicked(event)">Submit</button>
-
-JS:
-  function clicked(e)
-{
-    if(!confirm('Your message has been sent!')) {
-        e.preventDefault();
-    }
-}
-*/
-
 function validateForm() {
-  let validation1 = document.forms["myForm"]["fullname"].value;
-  let validation2 = document.forms["myForm"]["emailAddress"].value;
-  let validation3 = document.forms["myForm"]["phoneNumber"].value;
-  let validation4 = document.forms["myForm"]["messagebox"].value;
-  let redirect = "thankyou.html";
+  let redirect = window.location.href;
+  const newRedirect = redirect.replace("contact.html", "index.html");
+  window.location.href = newRedirect;
+  }
 
-  if (validation1 == "" || validation2 == "" || validation3 == "" || validation4 == "") {
-    alert("The form can't be sent because you haven't filled in all the fields!");
-    return false;
-  }
-  else {
-    alert("Your message has been sent!");
-    return true;
-  }
-}
+const submitButton = document.querySelector(".contact-submit-btn");
+submitButton.addEventListener("click", validateForm);
